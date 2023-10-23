b5e84b1ac87c601b4af0357868a62d1ae39486eb
simplypstar <97260718+simplypstar@users.noreply.github.com>
2023-10-23 17:24:16 -0400

    added function #2

--- functions_practice.py
+++ functions_practice.py
@@ -1,8 +1,18 @@
 #Local Python Setup Exercise
 
-# A function that prints a greeting to a user 
+# Practice function #1
+# Create a function that prints a greeting to a user 
 def hello():
     print("Greetings! Have a nice day!")
 
 hello()
 
+# Practice function #2
+# Create a function that accepts three arguments and returns a single list with
+# the three arguments as elements
+
+
+def pack(one, two, three):
+    return [one, two, three]
+
+print(pack("one", "two", "three"))
